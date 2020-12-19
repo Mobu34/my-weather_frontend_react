aaa5f6bcc5cf0269e2f3cc1d36673f45dd99efd5
Lucas Tidas <Lucas@macbook-pro-de-tidas.home>
2020-12-19 23:29:08 +0100

    geolocation init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [coords, setCoords] = useState({});
+
+  useEffect(() => {
+    console.log("useEffect");
+    navigator.geolocation.getCurrentPosition(({ coords }) => {
+      if (coords) {
+        setCoords(coords);
+      }
+    });
+  }, []);
+
+  console.log(coords);
   return (
     <div>
       Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
     </div>
   );
-}
+};
 
 export default App;
